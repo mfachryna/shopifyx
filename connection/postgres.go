d32ac8255274add6d0fca93dce488fe05fb349a7
Fachry <fachry.arifna@gmail.com>
2024-03-12 01:26:36 +0700

    init (DB Connection) : Create pgsql DB connection

---
+++ connection/postgres.go
@@ -0,0 +1,54 @@
+package connection
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	DbHost     string
+	DbPort     string
+	DbUsername string
+	DbName     string
+	DbPassword string
+}
+
+func openPg() (*sql.DB, error) {
+	conf := Config{
+		DbHost:     os.Getenv("DB_HOST"),
+		DbName:     os.Getenv("DB_NAME"),
+		DbPort:     os.Getenv("DB_PORT"),
+		DbUsername: os.Getenv("DB_USERNAME"),
+		DbPassword: os.Getenv("DB_PASSWORD"),
+	}
+
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		conf.DbUsername,
+		conf.DbPassword,
+		conf.DbHost,
+		conf.DbPort,
+		conf.DbName,
+	)
+
+	db, err := sql.Open("postgres", connStr)
+
+	return db, err
+}
+
+func SetupPg() *sql.DB {
+	db, err := openPg()
+	if err != nil {
+		return nil
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil
+	}
+	return db
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,7 @@
 module github.com/Croazt/shopifyx
 
-go 1.21.0
+go 1.19
 
-require (
-	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
-	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
-	github.com/klauspost/compress v1.17.7 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.52.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-)
+require github.com/lib/pq v1.10.9
+
+require github.com/joho/godotenv v1.5.1 // indirect
