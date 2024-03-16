6f9284416590b74d579687e7ed89d8a39ff539f2
Fachry <fachry.arifna@gmail.com>
2024-03-16 14:46:07 +0700

    refactor (bankAccountUpdate) : Change ID json from id to bankAccountId

--- domain/bank_accounts.go
+++ domain/bank_accounts.go
@@ -1,7 +1,7 @@
 package domain
 
 type BankAccount struct {
-	ID                string `json:"id"`
+	ID                string `json:"bankAccountId"`
 	BankName          string `json:"bankName" validate:"required,min=5,max=15"`
 	BankAccountName   string `json:"bankAccountName" validate:"required,min=5,max=15"`
 	BankAccountNumber string `json:"bankAccountNumber" validate:"required,min=5,max=60"`
